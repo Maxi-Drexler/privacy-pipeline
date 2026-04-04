4ab5434b9f734c6f83b3212e91149084308f6931
Maxi-Drexler <maxi.drexler@tum.de>
2026-04-04 20:31:04 +0200

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+"""
+Privacy-Aware Construction Site Monitoring Pipeline.
+
+A GDPR-compliant, open-source, on-premises system for detecting and
+anonymising privacy-sensitive regions in construction site camera footage.
+
+Author: Maximilian Drexler
+Institution: Technical University of Munich (TUM)
+Chair of Computational Modeling and Simulation (CMS)
+"""
+
+__version__ = "2.2.1"
+__author__ = "Maximilian Drexler"
